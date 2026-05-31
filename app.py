c310e8a67f216829eb71d5a63743ae52d40bed4c
maxxmoto <s.evsin@my18.ru>
2026-05-31 14:26:24 +0300

    Fix mailing: use client bot token instead of admin bot

--- app.py
+++ app.py
@@ -73,6 +73,7 @@ ADMIN_USERNAME = os.environ.get('ADMIN_USERNAME', 'admin')
 ADMIN_PASSWORD_HASH = generate_password_hash(os.environ.get('ADMIN_PASSWORD', 'vexio2024'))
 
 TG_TOKEN = os.environ.get('TG_TOKEN', '8868137062:AAH-_SKAP-poeG3afD3m47PAVhJbk8sqFl4')
+CLIENT_TG_TOKEN = os.environ.get('CLIENT_TG_TOKEN', '8964412503:AAEVNUajV66HteTLH8WuN-oHmquPt9IVDQo')
 TG_ADMIN_ID = os.environ.get('TG_ADMIN_ID', '903104535')
 
 def send_tg_notification(sub):
@@ -368,14 +369,14 @@ def admin_mailing():
             try:
                 if photo_bytes:
                     resp = requests.post(
-                        f'https://api.telegram.org/bot{TG_TOKEN}/sendPhoto',
+                        f'https://api.telegram.org/bot{CLIENT_TG_TOKEN}/sendPhoto',
                         files={'photo': (photo_filename, photo_bytes, photo_type)},
                         data={'chat_id': uid, 'caption': text, 'parse_mode': 'HTML'},
                         timeout=15,
                     )
                 else:
                     resp = requests.post(
-                        f'https://api.telegram.org/bot{TG_TOKEN}/sendMessage',
+                        f'https://api.telegram.org/bot{CLIENT_TG_TOKEN}/sendMessage',
                         json={'chat_id': uid, 'text': text, 'parse_mode': 'HTML'},
                         timeout=10,
                     )
